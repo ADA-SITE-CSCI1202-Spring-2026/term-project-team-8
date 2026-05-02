d39ac0ff09ea5df94c2dc007c791ff2884751066
fidankhalilova <fidankhalilova@Fidans-MacBook-Pro.local>
2026-05-02 12:37:11 +0400

    added run.sh for better access to final product

---
+++ AresBase/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+BASE=~/Desktop/ADA/term-project-team-8/AresBase
+FX=/Users/fidankhalilova/Downloads/javafx-sdk-26/lib
+MODS=javafx.controls,javafx.fxml,javafx.base,javafx.graphics
+
+echo "Compiling..."
+javac --module-path $FX --add-modules $MODS -d $BASE/out $(find $BASE/src -name "*.java")
+
+if [ $? -eq 0 ]; then
+    echo "Running..."
+    java --module-path $FX --add-modules $MODS -cp $BASE/out aresbase.Main
+else
+    echo "Compilation failed."
+fi
